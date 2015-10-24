6cc5602fc8f01014120c3d397acf8936e7daebe8
Paco Rampas <pacorampas@gmail.com>
2015-10-24 13:28:49 +0200

    refactor sticky directive

---
+++ src/directives/fm-sticky/fm-sticky.js
@@ -0,0 +1,40 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('flatMate')
+    .directive('fmSticky', fmSticky);
+
+  function fmSticky() {
+    return {
+      restrict: 'AE',
+      scope: {
+        top: '='
+      },
+      controller: fmStickyController
+    };
+  };
+
+  fmStickyController.$inject = [
+    '$scope',
+    '$element'
+  ];
+
+  function fmStickyController($scope, $element) {
+    if (!$scope.top) {
+      $scope.top = 0;
+    }
+
+    $element.on('scroll', function(event) {
+      var scroller = $element;
+      if (!scroller.hasClass('sticky-active') &&
+        scroller[0].scrollTop >= $scope.top) {
+        scroller.addClass('sticky-active');
+      } else if (scroller.hasClass('sticky-active') &&
+                 scroller[0].scrollTop <= $scope.top) {
+        scroller.removeClass('sticky-active');
+      }
+    })
+  }
+
+})();
